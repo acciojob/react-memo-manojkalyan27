d8c146a8b47553e9b570efde9eca7294cceec1d6
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-23 21:43:53 +0530

    Update todo.jsx

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,1 +1,31 @@
 
+import styles from "./todo.module.css";
+import { useState } from "react";
+
+
+const Todo =()=>{
+    const [todos,setTodo] = useState([]);
+    const AddTodo = ()=>{
+        setTodo([...todos,"New Todo"]);
+    };
+
+    return(       
+        <>
+            <div className={styles.title}>
+                <h1>React.useMemo</h1>
+                <h2>My todos</h2>
+            </div>
+
+            <div className={styles.displayMessage}>
+                {todos.map((todo,index)=>(
+                    <div className={styles.output} key={index}>{todo}</div>
+                ))}
+            </div>
+
+            <div>
+                <button onClick={AddTodo}>Add Todo</button>
+            </div>
+        </>
+    )
+}
+export default Todo;
