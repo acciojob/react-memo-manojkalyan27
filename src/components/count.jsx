211b747cc96c3d56448836602aa93a5d2313e063
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-23 21:44:40 +0530

    Update count.jsx

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,1 +1,22 @@
 
+import styles from "./count.module.css";
+import { useState } from "react";
+
+const Count = ()=>{
+    const [count, setCount] = useState(0)
+    const IncrementCount =()=>{
+        setCount(count + 1);
+    }
+    
+    return(
+        <div className={styles.container}>
+            <p className={styles.paraCount}>Count: {count} <button onClick={IncrementCount}>+</button></p>
+            <div className={styles.staticData}>
+                <h1 className={styles.heading}>Expensive Calculation</h1>
+                <span className={styles.figure}>100000000</span>
+            </div>
+        </div>
+    )
+}
+
+export default Count;
